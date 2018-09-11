9d02db68e947a7be9606ef505a9553fb4558cda7
abrsvn <abrsvn@gmail.com>
2018-09-10 20:09:31 -0700

    Updated info in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 VERSION = '0.2.3-1'
 
-setup(name='pyactr',
+setup(name='pyactr-book',
       version=VERSION,
-      description='ACT-R in Python',
-      url='https://github.com/jakdot/pyactr',
-      author='jakdot',
-      author_email='j.dotlacil@gmail.com',
-      packages=['pyactr', 'pyactr/tests'],
-      license='GPL',
+      description='ACT-R in Python3, package version and book code for the _Formal Linguistics and Cognitive Architecture_ book by Adrian Brasoveanu and Jakub Dotlacil',
+      url='https://github.com/jakdot/pyactr,https://github.com/abrsvn/pyactr-book',
+      author='jakdot,abrsvn',
+      author_email='j.dotlacil@gmail.com,abrsvn@gmail.com',
+      packages=['pyactr-book'],
       install_requires=['numpy', 'simpy', 'pyparsing'],
-      classifiers=['Programming Language :: Python :: 3', 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)', 'Operating System :: OS Independent', 'Development Status :: 3 - Alpha', 'Topic :: Scientific/Engineering'],
+      classifiers=['Programming Language :: Python :: 3', 'Operating System :: OS Independent', 'Development Status :: 3 - Alpha', 'Topic :: Scientific/Engineering'],
       zip_safe=False)
