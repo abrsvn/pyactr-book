df8a98db98125155bcc8a6a055aa03750db53741
abrsvn <abrsvn@gmail.com>
2018-09-10 20:11:58 -0700

    More updates to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ VERSION = '0.2.3-1'
 
 setup(name='pyactr-book',
       version=VERSION,
-      description='ACT-R in Python3, package version and book code for the _Formal Linguistics and Cognitive Architecture_ book by Adrian Brasoveanu and Jakub Dotlacil',
+      description='ACT-R in Python3, package frozen for the _Formal Linguistics and Cognitive Architecture_ book by Adrian Brasoveanu and Jakub Dotlacil; includes all code referred to in the book',
       url='https://github.com/jakdot/pyactr,https://github.com/abrsvn/pyactr-book',
       author='jakdot,abrsvn',
       author_email='j.dotlacil@gmail.com,abrsvn@gmail.com',
